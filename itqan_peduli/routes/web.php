52a88c5f76592b117ec49fb2162ac70ce3db51ba
Mugniraha <mughnirahadiansah@gmail.com>
2024-07-04 16:11:25 +0700

    create FE dashboard page

--- itqan_peduli/routes/web.php
+++ itqan_peduli/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\dashboardController;
+use App\Http\Controllers\transaksiController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('dashboard',dashboardController::class);
