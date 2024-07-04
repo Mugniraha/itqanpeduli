33a9be17474556564520708e181ea01f8aec61ec
Farhanf57 <farhanzoro283@gmail.com>
2024-07-04 17:08:27 +0700

    Layout admin

--- itqan_peduli/resources/views/admin/layout/main.blade.php
+++ itqan_peduli/resources/views/admin/layout/main.blade.php
@@ -8,8 +8,88 @@
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.css" rel="stylesheet" />
     <title>Document</title>
 </head>
-<body>
+<body class="w-full m-0 p-0">
+    <div class="bg-slate-50">
+        <div class="relative bg-transparent w-full">
+            <div id="sideBar" class="fixed bg-slate-200 w-64 h-full mx-4 mt-4 rounded-2xl">
+                <button onclick="closeNav()" type="button" class="items-center p-2 my-2 ms-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                    <svg class="w-[24px] h-[24px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m15 19-7-7 7-7"/>
+                    </svg>
+                </button>
+            </div>
+            <div id="main" class="relative ml-72 bg-transparent">
+                <div id="navBar" class="fixed bg-slate-50 w-[calc(100%-18rem)] h-20 flex content-center justify-between">
+                    <div class="flex items-center">
+                        <button id="iconNavOpen" onclick="openNav()" type="button" class="hidden items-center p-2 my-2 ms-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                            </svg>
+                        </button>
+                        <p class="font-bold text-lg text-gray-900 dark:text-white pl-5">Dashboard</p>
+                    </div>
+                    <div class="flex items-center">
+                        <div class="flex items-center ms-3">
+                            <div class="pr-5">
+                                <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
+                                <span class="sr-only">Open user menu</span>
+                                <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo">
+                                </button>
+                            </div>
+                            <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
+                                <div class="px-4 py-3" role="none">
+                                <p class="text-sm text-gray-900 dark:text-white" role="none">
+                                    Neil Sims
+                                </p>
+                                <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                                    neil.sims@flowbite.com
+                                </p>
+                                </div>
+                                <ul class="py-1" role="none">
+                                <li>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Dashboard</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Settings</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Earnings</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
+                                </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="bg-transparent pt-20">
+                    <div class="">
+                        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+                            @yield('konten')
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
+    <script>
+        function openNav() {
+            document.getElementById("sideBar").style.display = "block";
+            document.getElementById("iconNavOpen").style.display = "none";
+            document.getElementById("main").style.marginLeft = "18rem";
+            document.getElementById("navBar").style.width = "calc(100% - 18rem)";
+        }
+
+        function closeNav() {
+            document.getElementById("sideBar").style.display = "none";
+            document.getElementById("main").style.marginLeft = "0";
+            document.getElementById("navBar").style.width = "100%";
+            document.getElementById("iconNavOpen").style.display = "block";
+        }
+    </script>
 </body>
 </html>
--- itqan_peduli/routes/web.php
+++ itqan_peduli/routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.layout.main');
 });
+// Route::get('/publikasi-program', function () {
+//     return view('admin.konten.publikasiProgram');
+// });
