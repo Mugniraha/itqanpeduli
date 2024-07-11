88dd684ae2e469c18a7155bb2bed3af4a2c7df54
lutfhiemawarid@gmail.com <lutfhiemawarid@gmail.com>
2024-07-11 16:58:30 +0700

    fix footer

--- itqan_peduli/resources/views/front/konten/akun/main.blade.php
+++ itqan_peduli/resources/views/front/konten/akun/main.blade.php
@@ -234,14 +234,15 @@
                         <div class="p-4 md:p-5 ">
                             <p class="text-xl mt-8 font-semibold text-start">Yakin mau keluar aplikasi ?</p>
                             <h3 class="mb-5 mt-2 text-sm font-normal text-gray-500 text-start dark:text-gray-400">Apakah anda yakin keluar akun dari aplikasi ini ?</h3>
-                            <div class="text-center">
+                            <div class="text-center flex justify-around">
                                 <button data-modal-hide="popup-modal" type="button"
-                                class="py-2.5 px-5 ms-3 text-sm text-green-700 focus:outline-none bg-white rounded-lg border border-green-200 hover:bg-green-100 hover:text-green-900 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Ga, Keluar
-                            </button>
-                            <a href="{{  url('/login') }}" data-modal-hide="popup-modal" type="button"
-                                class="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-green-800 rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
-                                Ya, Keluar
-                            </a>
+                                class="py-2.5 px-5 shadow-md text-sm text-green-700 focus:outline-none bg-white rounded-lg hover:bg-green-100 hover:text-green-900 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                                    Gajadi Keluar
+                                </button>
+                                <a href="{{  url('/login') }}" data-modal-hide="popup-modal" type="button"
+                                    class="text-white shadow-md bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-green-800 rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                                    Ya , Keluar
+                                </a>
                             </div>
 
                         </div>
