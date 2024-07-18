ba1f8b6312308136ef9ba69db01f2d3f3460137d
Mugniraha <mughnirahadiansah@gmail.com>
2024-07-18 08:20:55 +0700

    perbaikan zakat

--- itqan_peduli/routes/web.php
+++ itqan_peduli/routes/web.php
@@ -237,29 +237,29 @@ Route::get('/ubah-profile', function () {
 //     return view('admin.isi.dashboard');
 // });
 
-Route::get('/dana', function () {
-    return view('admin.isi.dana');
-});
+// Route::get('/dana', function () {
+//     return view('admin.isi.dana');
+// });
 
-Route::get('/penyaluran', function () {
-    return view('admin.isi.penyaluran');
-});
+// Route::get('/penyaluran', function () {
+//     return view('admin.isi.penyaluran');
+// });
 
-Route::get('/lembaga', function () {
-    return view('admin.isi.lembaga');
-});
+// Route::get('/lembaga', function () {
+//     return view('admin.isi.lembaga');
+// });
 
-Route::get('/mediaberbagi', function () {
-    return view('admin.isi.mediaberbagi');
-});
+// Route::get('/mediaberbagi', function () {
+//     return view('admin.isi.mediaberbagi');
+// });
 
-Route::get('/berita', function () {
-    return view('admin.isi.berita');
-});
+// Route::get('/berita', function () {
+//     return view();
+// });
 
-Route::get('/kategori', function () {
-    return view('admin.isi.kategori');
-});
+// Route::get('/kategori', function () {
+//     return view('admin.isi.kategori');
+// });
 
 Route::get('/fundraiser', function () {
     return view('admin.fundraiser.fundraiser');
@@ -273,29 +273,29 @@ Route::get('/tranfun', function () {
     return view('admin.fundraiser.transaksi');
 });
 
-Route::get('/slider', function () {
-    return view('admin.web utama.slider');
-});
+// Route::get('/slider', function () {
+//     return view('admin.web utama.slider');
+// });
 
-Route::get('/partner', function () {
-    return view('admin.web utama.partner');
-});
+// Route::get('/partner', function () {
+//     return view('admin.web utama.partner');
+// });
 
-Route::get('/katblog', function () {
-    return view('admin.web utama.katblog');
-});
+// Route::get('/katblog', function () {
+//     return view('admin.web utama.katblog');
+// });
 
-Route::get('/blog', function () {
-    return view('admin.web utama.blog');
-});
+// Route::get('/blog', function () {
+//     return view('admin.web utama.blog');
+// });
 
-Route::get('/kegiatan', function () {
-    return view('admin.web utama.kegiatan');
-});
+// Route::get('/kegiatan', function () {
+//     return view('admin.web utama.kegiatan');
+// });
 
-Route::get('/konten', function () {
-    return view('admin.web utama.konten');
-});
+// Route::get('/konten', function () {
+//     return view('admin.web utama.konten');
+// });
 
 Route::get('/notifmail', function () {
     return view('admin.email.notif');
